154c348939655f1aceac8d62d7af4042ffe3f1dd
Adam Phillips <aphillips@scholastic.co.uk>
2012-07-19 00:06:07 +0100

    Added first test for History module
    This required some way of mocking the filesystem so FakeFS has been
    installed.

---
+++ test/test_console/history_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+require 'fakefs/safe'
+
+class TestConsole::HistoryTest < ActiveSupport::TestCase
+
+  context 'read' do
+    context 'when a file exists at the specified location' do
+      setup do
+        FakeFS.activate!
+        FileUtils.mkdir_p File.dirname(TestConsole::History::LOCATION)
+        File.open(TestConsole::History::LOCATION, 'w+') do |file|
+          Marshal.dump(['a command'], file)
+        end
+      end
+
+      teardown do
+        FakeFS.deactivate!
+      end
+
+      should 'read the file and load the lines from the history into the readline history' do
+        assert File.exists?(TestConsole::History::LOCATION)
+        TestConsole::History.read
+        assert_equal 'a command', Readline::HISTORY.to_a.last
+      end
+    end
+  end
+
+end
