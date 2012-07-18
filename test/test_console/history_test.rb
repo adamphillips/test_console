cf19b823e0779e772f72d01deeb87fd24e4f6e13
Adam Phillips <aphillips@scholastic.co.uk>
2012-07-19 00:34:22 +0100

    Fixed FakeFS issues
    Had filesystem issues after mocking with FakeFS.

--- test/test_console/history_test.rb
+++ test/test_console/history_test.rb
@@ -9,8 +9,8 @@ class TestConsole::HistoryTest < ActiveSupport::TestCase
       setup do
         FakeFS.activate!
         FileUtils.mkdir_p File.dirname(TestConsole::History::LOCATION)
-        File.open(TestConsole::History::LOCATION, 'w+') do |file|
-          Marshal.dump(['a command'], file)
+        File.open(TestConsole::History::LOCATION, 'w') do |file|
+          Marshal.dump(['cmd', '123', 'abc'], file)
         end
       end
 
@@ -19,9 +19,11 @@ class TestConsole::HistoryTest < ActiveSupport::TestCase
       end
 
       should 'read the file and load the lines from the history into the readline history' do
+        # Note that there is an issue with the Readline history in that the first item never appears
+        # http://bogojoker.com/readline/#libedit_history_issue
         assert File.exists?(TestConsole::History::LOCATION)
         TestConsole::History.read
-        assert_equal 'a command', Readline::HISTORY.to_a.last
+        assert_equal ['123', 'abc'], Readline::HISTORY.to_a
       end
     end
   end
